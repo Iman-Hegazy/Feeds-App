f130741f7a2c5c138836542e5be94eb3d016093e
Eman Hegazy <iman.hegazy@vodafone.com>
2023-05-28 16:52:04 +0300

    remove unneeded lines

--- util/ExternalUrlNavigation.tsx
+++ util/ExternalUrlNavigation.tsx
@@ -3,8 +3,3 @@ import { Linking } from "react-native";
 export const openInNewTab = (url: string): void => {
   Linking.openURL(url).catch((err) => console.error("An error occurred", err));
 };
-
-// export const onClickUrl =
-//   (url: string): (() => void) =>
-//   () =>
-//     openInNewTab(url);
